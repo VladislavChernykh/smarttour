eb119e69e5a3d23644d7fa6fc0fab99e4583fbeb
Vladislav Chernykh <vladislav_chernykh@epam.com>
2021-06-20 10:47:53 +0300

    Add user generation

--- service/apis/users.py
+++ service/apis/users.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Blueprint, jsonify
 
 users = Blueprint("users", __name__)
@@ -5,19 +7,16 @@ users = Blueprint("users", __name__)
 
 @users.route("/all", methods=["GET"])
 def get_all_users():
-    users_mock = [
-        {
-            "id": 1,
-            "first_name": "Олег",
-            "last_name": "Олегов",
-        },
-        {
-            "id": 2,
-            "first_name": "Иван",
-            "last_name": "Иванов",
+    users_mock = []
+    fnames = ["Олег", "Иван", "Андрей", "Николай", "Владислав", "Вячеслав", "Владимир", "Алексей", "Митрофан",
+              "Аркадий", "Семён", "Илья", "Юрий"]
+    lnames = ["Беляев", "Чернов", "Агутин", "Малинин", "Краснощёков", "Иванов", "Запашный", "Хорин", "Алексеев",
+              "Желтов", "Краснов", "Прохоров"]
+    for i in range(100):
+        user = {
+            "id": i,
+            "first_name": random.choice(fnames),
+            "last_name": random.choice(lnames),
         }
-    ]
+        users_mock.append(user)
     return jsonify(users_mock)
-
-# @locations.route("/get_by_category", methods=["GET"])
-# def get_all_locations():
